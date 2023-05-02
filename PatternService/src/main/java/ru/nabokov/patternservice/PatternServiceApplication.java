6ceb0b4bad421aac00bb5a4545a92657bee44f8a
NabokovSG <nabokovsg@yandex.ru>
2023-05-02 10:16:06 +0300

    add start file

---
+++ PatternService/src/main/java/ru/nabokov/patternservice/PatternServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.nabokov.patternservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatternServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatternServiceApplication.class, args);
+    }
+
+}
