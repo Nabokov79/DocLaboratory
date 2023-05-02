e0038c281ff486f21ec308d52c48cffd2539396c
NabokovSG <nabokovsg@yandex.ru>
2023-05-02 11:22:53 +0300

    add dto for client

---
+++ PassportService/src/main/java/ru/nabokov/passportservice/dto/client/TypeDto.java
@@ -0,0 +1,16 @@
+package ru.nabokov.passportservice.dto.client;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TypeDto {
+
+    private long id;
+    private String name;
+}
