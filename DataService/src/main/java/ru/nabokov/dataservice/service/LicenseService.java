089f8e92dffebacdb31a227933bbe98a0ced9b6d
NabokovSG <nabokovsg@yandex.ru>
2023-05-05 15:36:03 +0300

    no commit

--- DataService/src/main/java/ru/nabokov/dataservice/service/LicenseService.java
+++ DataService/src/main/java/ru/nabokov/dataservice/service/LicenseService.java
@@ -3,6 +3,7 @@ package ru.nabokov.dataservice.service;
 import ru.nabokov.dataservice.dto.license.LicenseDto;
 import ru.nabokov.dataservice.dto.license.NewLicenseDto;
 import ru.nabokov.dataservice.dto.license.UpdateLicenseDto;
+import ru.nabokov.dataservice.model.Licenses;
 import java.util.List;
 
 public interface LicenseService {
@@ -11,7 +12,7 @@ public interface LicenseService {
 
     LicenseDto update(UpdateLicenseDto licenseDto);
 
-    LicenseDto get(Long id);
+    Licenses get(Long id);
 
     List<LicenseDto> getAll();
 }
