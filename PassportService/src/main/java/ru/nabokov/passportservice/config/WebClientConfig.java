83f12346eb103cbac63353bc3f71a0ecb89d92be
NabokovSG <nabokovsg@yandex.ru>
2023-05-05 15:48:43 +0300

    no commit

--- PassportService/src/main/java/ru/nabokov/passportservice/config/WebClientConfig.java
+++ PassportService/src/main/java/ru/nabokov/passportservice/config/WebClientConfig.java
@@ -1,4 +1,4 @@
-package ru.nabokov.passportservice.client;
+package ru.nabokov.passportservice.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
