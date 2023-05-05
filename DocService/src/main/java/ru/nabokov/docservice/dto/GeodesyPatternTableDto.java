7b463dbb864431d33cb1073a7f495b9631b59eaf
NabokovSG <nabokovsg@yandex.ru>
2023-05-05 15:57:47 +0300

    add dto

---
+++ DocService/src/main/java/ru/nabokov/docservice/dto/GeodesyPatternTableDto.java
@@ -0,0 +1,27 @@
+package ru.nabokov.docservice.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.List;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class GeodesyPatternTableDto {
+
+    private long id;
+    private String name;
+    private List<ColumnHeaderDto> columnHeaders;
+    private GeodesyCombinedColumnsDto combinedColumn;
+
+    @Override
+    public String toString() {
+        return "GeodesyPatternTableDto{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", columnHeaders=" + columnHeaders +
+                ", combinedColumn=" + combinedColumn +
+                '}';
+    }
+}
