d095cabd9f79ab78e8601e568920fa8f3780fd98
NabokovSG <nabokovsg@yandex.ru>
2023-05-05 15:35:01 +0300

    no commit

--- DataService/src/main/java/ru/nabokov/dataservice/repository/LicenseRepository.java
+++ DataService/src/main/java/ru/nabokov/dataservice/repository/LicenseRepository.java
@@ -1,10 +1,10 @@
 package ru.nabokov.dataservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import ru.nabokov.dataservice.model.License;
+import ru.nabokov.dataservice.model.Licenses;
 import java.time.LocalDate;
 
-public interface LicenseRepository extends JpaRepository<License, Long> {
+public interface LicenseRepository extends JpaRepository<Licenses, Long> {
 
     boolean existsByDate(LocalDate date);
 }
