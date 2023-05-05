ea69d5cbceaf4139ff576893f0a421eddbbc8595
NabokovSG <nabokovsg@yandex.ru>
2023-05-05 15:52:58 +0300

    add config for webPatternClient and webDataClient

---
+++ DocService/src/main/java/ru/nabokov/docservice/config/WebClientConfig.java
@@ -0,0 +1,29 @@
+package ru.nabokov.docservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Value("${pattern-service.url}")
+    private String patternUrl;
+    @Value("${data-service.url}")
+    private String dataUrl;
+
+    @Bean
+    public WebClient webPatternClient() {
+        return WebClient.builder()
+                .baseUrl(patternUrl)
+                .build();
+    }
+
+    @Bean
+    public WebClient webDataClient() {
+        return WebClient.builder()
+                .baseUrl(dataUrl)
+                .build();
+    }
+}
