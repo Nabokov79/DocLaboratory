00f80ed517635d43b8b692336dcb54243ce1b5f1
NabokovSG <nabokovsg@yandex.ru>
2023-05-05 15:55:06 +0300

    add client

---
+++ DocService/src/main/java/ru/nabokov/docservice/client/PatternClient.java
@@ -0,0 +1,30 @@
+package ru.nabokov.docservice.client;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import ru.nabokov.docservice.dto.ReportPatternDto;
+
+import java.util.Objects;
+
+@Component
+public class PatternClient {
+
+    private final WebClient webClient;
+
+    public PatternClient(@Qualifier("webPatternClient") WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public ReportPatternDto getReportPatternDto(String path, Long typeId) {
+        return Objects.requireNonNull(webClient.get()
+                        .uri(uriBuilder -> uriBuilder
+                                .path(path)
+                                .queryParam("typeId", typeId)
+                                .build())
+                        .retrieve()
+                        .toEntity(ReportPatternDto.class)
+                        .block())
+                .getBody();
+    }
+}
