98d89e19eaace8512df9c073851174827b88cbd4
Nabokov79 <nabokovsg@yandex.ru>
2023-05-08 16:41:04 +0300

    instead SurveyDto -> Survey

--- PassportService/src/main/java/ru/nabokov/passportservice/service/SurveyService.java
+++ PassportService/src/main/java/ru/nabokov/passportservice/service/SurveyService.java
@@ -1,15 +1,14 @@
 package ru.nabokov.passportservice.service;
 
 import ru.nabokov.passportservice.dto.survey.NewSurveyDto;
-import ru.nabokov.passportservice.dto.survey.SurveyDto;
 import ru.nabokov.passportservice.dto.survey.UpdateSurveyDto;
-import ru.nabokov.passportservice.model.Passport;
+import ru.nabokov.passportservice.model.Survey;
 
 import java.util.List;
 
 public interface SurveyService {
 
-    List<SurveyDto> save(Passport passport, List<NewSurveyDto> surveysDto);
+    List<Survey> save(List<NewSurveyDto> surveysDto);
 
-    List<SurveyDto> update(Passport passport, List<UpdateSurveyDto> surveysDto);
+    List<Survey> update(List<UpdateSurveyDto> surveysDto);
 }
