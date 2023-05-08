3f51eba4c8030adbe5771e14cfe1cfda06533526
Nabokov79 <nabokovsg@yandex.ru>
2023-05-08 16:39:13 +0300

    delete methods validate(), update methods saveAll, updateAll

--- PatternService/src/main/java/ru/nabokov/patternservice/service/SubheadingServiceImpl.java
+++ PatternService/src/main/java/ru/nabokov/patternservice/service/SubheadingServiceImpl.java
@@ -2,8 +2,8 @@ package ru.nabokov.patternservice.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.nabokov.patternservice.exceptions.BadRequestException;
 import ru.nabokov.patternservice.exceptions.NotFoundException;
+import ru.nabokov.patternservice.model.Documentation;
 import ru.nabokov.patternservice.model.Subheading;
 import ru.nabokov.patternservice.repository.SubheadingRepository;
 import java.util.ArrayList;
@@ -17,46 +17,50 @@ public class SubheadingServiceImpl implements SubheadingService {
 
     private final SubheadingRepository repository;
     private final DocumentationService documentationService;
+    private final ColumnHeaderService columnHeaderService;
 
     @Override
     public List<Subheading> saveAll(List<Subheading> subheadings) {
         for (Subheading subheading : subheadings) {
-            validate(subheading);
+            if (!subheading.getColumnHeaders().isEmpty()) {
+                subheading.setColumnHeaders(columnHeaderService.save(subheading.getColumnHeaders()));
+            }
         }
-        List<Subheading> subheadingsDb = repository.saveAll(subheadings);
-        documentationService.save(subheadings, subheadingsDb.stream()
-                                                            .collect(Collectors.toMap(Subheading::getHeading, s -> s)));
-        return repository.findAllById(subheadingsDb.stream().map(Subheading::getId).toList());
+        List<Subheading> subheading = repository.saveAll(subheadings);
+        Map<Double, Subheading> subheadingsDb = subheading.stream()
+                                                          .filter(s -> s.getDocumentations() != null)
+                                                          .collect(Collectors.toMap(Subheading::getNumber, s -> s));
+        Map<Double,List<Documentation>> documentations = subheadings
+                                       .stream()
+                                       .filter(s -> s.getDocumentations() != null)
+                                       .collect(Collectors.toMap(Subheading::getNumber, Subheading::getDocumentations));
+        for (Double number : subheadings.stream().filter(s -> s.getDocumentations() != null)
+                                                 .map(Subheading::getNumber)
+                                                 .toList()){
+            documentationService.save(subheadingsDb.get(number), documentations.get(number));
+        }
+        return subheading;
     }
 
     @Override
     public List<Subheading> updateAll(List<Subheading> subheadings) {
         validateIds(subheadings.stream().map(Subheading::getId).toList());
         for (Subheading subheading : subheadings) {
-            validate(subheading);
-            if (subheading.getDocumentations() != null && !subheading.getDocumentations().isEmpty()) {
-                documentationService.update(subheading.getDocumentations());
+            if (!subheading.getColumnHeaders().isEmpty()) {
+                subheading.setColumnHeaders(columnHeaderService.update(subheading.getColumnHeaders()));
             }
         }
-        return repository.saveAll(subheadings);
-    }
-
-    private void validate(Subheading subheading) {
-        if (subheading.getNumber() == null) {
-            throw new BadRequestException("number subheading should not be blank");
+        List<Subheading> subheading = repository.saveAll(subheadings);
+        Map<Double,List<Documentation>> documentations = subheadings
+                .stream()
+                .filter(s -> s.getDocumentations() != null)
+                .collect(Collectors.toMap(Subheading::getNumber, Subheading::getDocumentations));
+        for (Double number : subheadings.stream().filter(s -> s.getDocumentations() != null)
+                .map(Subheading::getNumber)
+                .toList()){
+            documentationService.update(documentations.get(number));
         }
-        if (subheading.getNumber() <= 0) {
-            throw new BadRequestException("number subheading can only be positive");
-        }
-        if (subheading.getHeading() == null) {
-            throw new BadRequestException("heading subheading should not be blank");
-        }
-        if (subheading.getHeading().length() < 1) {
-            throw new BadRequestException("length of the subtitle of the title should not be less than one");
-        }
-        if (subheading.getText() != null && subheading.getText().length() < 1) {
-            throw new BadRequestException("length of the text of the subtitle of the title cannot be less than one");
-        }
+        return subheading;
     }
 
     private void validateIds(List<Long> ids) {
