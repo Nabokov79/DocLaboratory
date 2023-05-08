6e65d7d4a7a8ce21337e09b93958202e5435a82c
Nabokov79 <nabokovsg@yandex.ru>
2023-05-08 16:35:23 +0300

    delete method existsAllByPassport

--- PassportService/src/main/java/ru/nabokov/passportservice/repository/RepairRepository.java
+++ PassportService/src/main/java/ru/nabokov/passportservice/repository/RepairRepository.java
@@ -1,10 +1,7 @@
 package ru.nabokov.passportservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import ru.nabokov.passportservice.model.Passport;
 import ru.nabokov.passportservice.model.Repair;
 
 public interface RepairRepository extends JpaRepository<Repair, Long> {
-
-    boolean existsAllByPassport(Passport passport);
 }
