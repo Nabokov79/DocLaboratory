0dad971c26daf2a31b64e1c7944f02806475b736
Nabokov79 <nabokovsg@yandex.ru>
2023-05-08 16:20:03 +0300

    update method save()

--- PatternService/src/main/java/ru/nabokov/patternservice/service/DocumentationService.java
+++ PatternService/src/main/java/ru/nabokov/patternservice/service/DocumentationService.java
@@ -3,11 +3,10 @@ package ru.nabokov.patternservice.service;
 import ru.nabokov.patternservice.model.Documentation;
 import ru.nabokov.patternservice.model.Subheading;
 import java.util.List;
-import java.util.Map;
 
 public interface DocumentationService {
 
-    void save(List<Subheading> subheadings, Map<String, Subheading> subheadingsDb);
+    void save(Subheading subheading, List<Documentation> documentations);
 
     void update(List<Documentation> documentations);
 }
--- PatternService/src/main/java/ru/nabokov/patternservice/service/DocumentationServiceImpl.java
+++ PatternService/src/main/java/ru/nabokov/patternservice/service/DocumentationServiceImpl.java
@@ -7,6 +7,7 @@ import ru.nabokov.patternservice.exceptions.NotFoundException;
 import ru.nabokov.patternservice.model.Documentation;
 import ru.nabokov.patternservice.model.Subheading;
 import ru.nabokov.patternservice.repository.DocumentationRepository;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -19,25 +20,12 @@ public class DocumentationServiceImpl implements DocumentationService {
     private final DocumentationRepository repository;
 
     @Override
-    public void save(List<Subheading> subheadings, Map<String, Subheading> subheadingsDb) {
-        Map<Double,List<Documentation>> documentationsDto = subheadings
-                .stream()
-                .filter(s -> s.getDocumentations() != null)
-                .collect(Collectors.toMap(Subheading::getNumber, Subheading::getDocumentations));
-        if (!documentationsDto.isEmpty()) {
-            List<Documentation> documentation = new ArrayList<>();
-            for (Subheading subheading: subheadingsDb.values()) {
-                List<Documentation> documentations = documentationsDto.get(subheading.getNumber());
-                if (documentations != null) {
-                    for (Documentation document : documentations) {
-                        validate(document);
-                        document.setSubheading(subheadingsDb.get(subheading.getHeading()));
-                        documentation.add(document);
-                    }
-                }
-            }
-            repository.saveAll(documentation);
+    public void save(Subheading subheading, List<Documentation> documentations) {
+        for (Documentation document : documentations) {
+            validate(document);
+            document.setSubheading(subheading);
         }
+        repository.saveAll(documentations);
     }
 
     @Override
@@ -53,7 +41,7 @@ public class DocumentationServiceImpl implements DocumentationService {
         if (document.getTypeDocument() == null) {
             throw new BadRequestException("type document should not be blank");
         }
-        if (!document.getTypeDocument().isBlank()  && document.getTypeDocument().length() < 1) {
+        if (!document.getTypeDocument().isBlank() && document.getTypeDocument().length() < 1) {
             throw new BadRequestException("length of the document type cannot be less than one");
         }
         if (document.getNumberDocument() != null && document.getNumberDocument().length() < 1) {
