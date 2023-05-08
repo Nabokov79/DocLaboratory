9b6c7008cd66b0aabc24b14a3b809d8ebbcbb25a
Nabokov79 <nabokovsg@yandex.ru>
2023-05-08 16:57:25 +0300

    git status - delete

--- PatternService/src/main/java/ru/nabokov/patternservice/repository/TitlePageDataRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.nabokov.patternservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.nabokov.patternservice.model.TitlePageData;
-
-public interface TitlePageDataRepository extends JpaRepository<TitlePageData, Long> {
-}
