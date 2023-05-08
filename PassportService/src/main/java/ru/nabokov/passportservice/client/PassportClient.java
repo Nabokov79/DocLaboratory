94eb90b6a2564337bd70c7cbdfe902b5258c926e
Nabokov79 <nabokovsg@yandex.ru>
2023-05-08 16:23:26 +0300

    delete method getType

--- PassportService/src/main/java/ru/nabokov/passportservice/client/PassportClient.java
+++ PassportService/src/main/java/ru/nabokov/passportservice/client/PassportClient.java
@@ -4,8 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.nabokov.passportservice.dto.client.ObjectDataDto;
 import ru.nabokov.passportservice.dto.client.OrganizationDto;
-import ru.nabokov.passportservice.dto.client.TypeDto;
-
 import java.util.List;
 
 @Service
@@ -16,10 +14,6 @@ public class PassportClient {
 
     public ObjectDataDto getObjectData(Long id) {
         return client.getObjectData("/data/object/" + id);
-    }
-
-    public TypeDto getType(Long id) {
-        return client.getType("/data/type/" + id);
     }
 
     public List<OrganizationDto> getOrganizations(String ids) {
