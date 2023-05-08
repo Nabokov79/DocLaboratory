0556f994a9d483f58bd9b24c2ef58b3bd0eda2b5
Nabokov79 <nabokovsg@yandex.ru>
2023-05-08 16:36:41 +0300

    instead RepairDto -> Repair

--- PassportService/src/main/java/ru/nabokov/passportservice/service/RepairService.java
+++ PassportService/src/main/java/ru/nabokov/passportservice/service/RepairService.java
@@ -1,15 +1,14 @@
 package ru.nabokov.passportservice.service;
 
 import ru.nabokov.passportservice.dto.repair.NewRepairDto;
-import ru.nabokov.passportservice.dto.repair.RepairDto;
 import ru.nabokov.passportservice.dto.repair.UpdateRepairDto;
-import ru.nabokov.passportservice.model.Passport;
+import ru.nabokov.passportservice.model.Repair;
 
 import java.util.List;
 
 public interface RepairService {
 
-    List<RepairDto> save(Passport passport, List<NewRepairDto> repairsDto);
+    List<Repair> save(List<NewRepairDto> repairsDto);
 
-    List<RepairDto> update(Passport passport, List<UpdateRepairDto> repairDto);
+    List<Repair> update(List<UpdateRepairDto> repairDto);
 }
