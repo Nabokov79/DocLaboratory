b633c1638d9a1a42b03973d930686a66778795cb
Nabokov79 <nabokovsg@yandex.ru>
2023-05-08 16:25:59 +0300

    add SurveyMapper, SurveyService, RepairMapper, RepairService

--- PassportService/src/main/java/ru/nabokov/passportservice/service/PassportServiceImpl.java
+++ PassportService/src/main/java/ru/nabokov/passportservice/service/PassportServiceImpl.java
@@ -4,18 +4,26 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.nabokov.passportservice.client.PassportClient;
 import ru.nabokov.passportservice.dto.client.ObjectDataDto;
+import ru.nabokov.passportservice.dto.client.OrganizationDto;
 import ru.nabokov.passportservice.dto.passport.NewPassportDto;
 import ru.nabokov.passportservice.dto.passport.ShortPassportDto;
 import ru.nabokov.passportservice.dto.passport.PassportDto;
 import ru.nabokov.passportservice.dto.passport.UpdatePassportDto;
+import ru.nabokov.passportservice.dto.repair.RepairDto;
+import ru.nabokov.passportservice.dto.survey.SurveyDto;
 import ru.nabokov.passportservice.exceptions.BadRequestException;
 import ru.nabokov.passportservice.exceptions.NotFoundException;
 import ru.nabokov.passportservice.mapper.PassportMapper;
+import ru.nabokov.passportservice.mapper.RepairMapper;
+import ru.nabokov.passportservice.mapper.SurveyMapper;
 import ru.nabokov.passportservice.model.Passport;
+import ru.nabokov.passportservice.model.Repair;
+import ru.nabokov.passportservice.model.Survey;
 import ru.nabokov.passportservice.repository.PassportRepository;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -23,8 +31,12 @@ public class PassportServiceImpl implements PassportService {
 
     private final PassportRepository repository;
     private final PassportMapper mapper;
+    private final SurveyMapper surveyMapper;
+    private final RepairMapper repairMapper;
     private final PassportClient client;
     private final CharacteristicService characteristicService;
+    private final SurveyService surveyService;
+    private final RepairService repairService;
 
     @Override
     public PassportDto save(NewPassportDto passportDto) {
@@ -35,10 +47,11 @@ public class PassportServiceImpl implements PassportService {
         Passport passport = mapper.mapFromNewPassportDto(passportDto);
         ObjectDataDto objectData = client.getObjectData(passportDto.getObjectDataId());
         passport.setTypeId(objectData.getType().getId());
-        PassportDto passportDTO = mapper.mapToPassportDto(repository.save(passport));
-        passportDTO.setObjectData(objectData);
-        passportDTO.setCharacteristics(characteristicService.save(passport, passportDto.getCharacteristics()));
-        return passportDTO;
+        passport.setObjectDataId(objectData.getId());
+        characteristicService.save(passport, passportDto.getCharacteristics());
+        passport.setRepairs(repairService.save(passportDto.getRepairs()));
+        passport.setSurveys(surveyService.save(passportDto.getSurveys()));
+        return setValues(repository.save(passport), objectData);
     }
 
     @Override
@@ -49,9 +62,11 @@ public class PassportServiceImpl implements PassportService {
         Passport passport = mapper.mapFromUpdatePassportDto(passportDto);
         ObjectDataDto objectData = client.getObjectData(passportDto.getObjectDataId());
         passport.setTypeId(objectData.getType().getId());
-        PassportDto passportDTO = mapper.mapToPassportDto(repository.save(passport));
-        passportDTO.setObjectData(objectData);
-        return passportDTO;
+        passport.setObjectDataId(objectData.getId());
+        characteristicService.update(passport, passportDto.getCharacteristics());
+        passport.setRepairs(repairService.update(passportDto.getRepairs()));
+        passport.setSurveys(surveyService.update(passportDto.getSurveys()));
+        return setValues(repository.save(passport), objectData);
     }
 
     @Override
@@ -81,5 +96,32 @@ public class PassportServiceImpl implements PassportService {
             return;
         }
         throw new NotFoundException(String.format("Passports with id=%s not found for delete", id));
+    }
+
+    private PassportDto setValues(Passport passport, ObjectDataDto objectData) {
+        PassportDto passportDto = mapper.mapToPassportDto(repository.save(passport));
+        Map<Long, OrganizationDto> organizations = client.getOrganizations(
+                String.join(",", String.valueOf(passport.getSurveys().stream().map(Survey::getOrganizationId).distinct()),
+                                         String.valueOf(passport.getRepairs().stream().map(Repair::getOrganizationId).distinct()))
+                ).stream().collect(Collectors.toMap(OrganizationDto::getId, o -> o));
+        if (organizations.isEmpty()) {
+            throw new NotFoundException("organizations for surveys and repairs not found");
+        }
+        List<RepairDto> repairsDto = new ArrayList<>();
+        for (Repair repair : passport.getRepairs()) {
+            RepairDto repairDto = repairMapper.mapToRepairsDto(repair);
+            repairDto.setOrganization(organizations.get(repair.getOrganizationId()));
+            repairsDto.add(repairDto);
+        }
+        List<SurveyDto> surveysDto = new ArrayList<>();
+        for (Survey survey : passport.getSurveys()) {
+            SurveyDto surveyDto = surveyMapper.mapToSurveysDto(survey);
+            surveyDto.setOrganization(organizations.get(survey.getOrganizationId()));
+            surveysDto.add(surveyDto);
+        }
+        passportDto.setRepairs(repairsDto);
+        passportDto.setSurveys(surveysDto);
+        passportDto.setObjectData(objectData);
+        return passportDto;
     }
 }
