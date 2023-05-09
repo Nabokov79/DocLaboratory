118ffda3b295854aa40c5e19f5b3760fe533d099
Nabokov79 <nabokovsg@yandex.ru>
2023-05-09 19:53:15 +0300

    without changes

--- DocService/src/main/java/ru/nabokov/docservice/service/FirstSectionService.java
+++ DocService/src/main/java/ru/nabokov/docservice/service/FirstSectionService.java
@@ -1,10 +1,13 @@
 package ru.nabokov.docservice.service;
 
+import ru.nabokov.docservice.dto.EmployeeDto;
 import ru.nabokov.docservice.dto.PatternSectionOneDto;
 import ru.nabokov.docservice.dto.title.BranchDto;
 import ru.nabokov.docservice.model.FirstSection;
 
+import java.util.List;
+
 public interface FirstSectionService {
 
-    FirstSection save(BranchDto branch, PatternSectionOneDto pattern);
+    FirstSection save(BranchDto branch, PatternSectionOneDto pattern, List<EmployeeDto> employees, String license);
 }
--- DocService/src/main/java/ru/nabokov/docservice/service/FirstSectionServiceImpl.java
+++ DocService/src/main/java/ru/nabokov/docservice/service/FirstSectionServiceImpl.java
@@ -2,10 +2,13 @@ package ru.nabokov.docservice.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.nabokov.docservice.dto.EmployeeDto;
 import ru.nabokov.docservice.dto.PatternSectionOneDto;
 import ru.nabokov.docservice.dto.title.BranchDto;
 import ru.nabokov.docservice.model.FirstSection;
 import ru.nabokov.docservice.repository.FirstSectionRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,11 +18,11 @@ public class FirstSectionServiceImpl implements FirstSectionService {
     private final DataFirstSectionService dataFirstSectionService;
 
     @Override
-    public FirstSection save(BranchDto branch, PatternSectionOneDto pattern) {
+    public FirstSection save(BranchDto branch, PatternSectionOneDto pattern, List<EmployeeDto> employees, String license) {
         FirstSection section = new FirstSection();
         section.setHeading(String.join(". ", String.valueOf(pattern.getHeader().getNumber()),
                                                       pattern.getHeader().getHeading()));
-        dataFirstSectionService.save(repository.save(section), pattern.getSubheadings(), branch);
+        dataFirstSectionService.save(repository.save(section), pattern.getSubheadings(), branch, employees, license);
         return null;
     }
 }
