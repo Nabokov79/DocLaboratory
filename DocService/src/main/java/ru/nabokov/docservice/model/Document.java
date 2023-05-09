dac86a90159a3382dda514326be49c1407aa9851
Nabokov79 <nabokovsg@yandex.ru>
2023-05-09 19:51:42 +0300

    delete field  - typeId

--- DocService/src/main/java/ru/nabokov/docservice/model/Document.java
+++ DocService/src/main/java/ru/nabokov/docservice/model/Document.java
@@ -18,8 +18,6 @@ public class Document {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    @Column(name = "type_id")
-    private Long typeId;
     @Column(name = "document")
     private String document;
 }
