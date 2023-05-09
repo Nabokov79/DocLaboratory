2d79191d5a0848d29ee83694a440db67027a0f27
Nabokov79 <nabokovsg@yandex.ru>
2023-05-09 19:51:05 +0300

    add method save

--- DocService/src/main/java/ru/nabokov/docservice/service/DataFirstSectionServiceImpl.java
+++ DocService/src/main/java/ru/nabokov/docservice/service/DataFirstSectionServiceImpl.java
@@ -2,23 +2,149 @@ package ru.nabokov.docservice.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.nabokov.docservice.dto.SubheadingDto;
+import ru.nabokov.docservice.dto.*;
 import ru.nabokov.docservice.dto.title.BranchDto;
+import ru.nabokov.docservice.dto.title.OrganizationDto;
 import ru.nabokov.docservice.mapper.SectionMapper;
 import ru.nabokov.docservice.model.DataFirstSection;
+import ru.nabokov.docservice.model.Document;
 import ru.nabokov.docservice.model.FirstSection;
 import ru.nabokov.docservice.repository.DataFirstSectionRepository;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class DataFirstSectionServiceImpl implements DataFirstSectionService {
 
+    private static final String FROM = "от";
+    private static final String ISSUED = "выдано";
+    private static final String LEVEL_TEXT = "уровень квалификации по";
     private final DataFirstSectionRepository repository;
     private final SectionMapper mapper;
+    private final StringService stringService;
 
     @Override
-    public void save(FirstSection section, List<SubheadingDto> subheadings, BranchDto branch) {
-        List<DataFirstSection> dataSection = mapper.mapToDataFirstSection(subheadings);
+    public void save(FirstSection section, List<SubheadingDto> subheadingsDto, BranchDto branch, List<EmployeeDto> employeesDto, String license) {
+        Map<Double, DataFirstSection> dataSections = mapper.mapToDataFirstSection(subheadingsDto)
+                                                        .stream()
+                                                        .collect(Collectors.toMap(DataFirstSection::getNumber, d -> d));
+        Map<Double, SubheadingDto> subheadings = subheadingsDto.stream()
+                                                           .collect(Collectors.toMap(SubheadingDto::getNumber, s -> s));
+        for (Double number : dataSections.keySet()) {
+            DataFirstSection data = dataSections.get(number);
+            if (data.getText() == null && data.getDocumentations() == null) {
+                data.setLaboratoryData(setLaboratoryData(branch, license));
+                String employeeFirst = "";
+                String employeeSecond = "";
+                for (EmployeeDto employee : employeesDto) {
+                    if (employeeFirst.isBlank()) {
+                        employeeFirst = getStringEmployees(employee);
+                    } else {
+                        employeeSecond = getStringEmployees(employee);
+                    }
+                }
+                data.setEmployeeFirst(employeeFirst);
+                data.setEmployeeSecond(employeeSecond);
+            }
+            if (data.getDocumentations() != null) {
+                data.setDocumentations(toStringDocumentation(subheadings.get(number).getDocumentations()));
+            }
+            data.setSection(section);
+            dataSections.put(number, data);
+        }
+        repository.saveAll(new ArrayList<>(dataSections.values()));
+    }
+
+    private List<Document> toStringDocumentation(List<DocumentationDto> documentationsDto) {
+        List<Document> documentations = new ArrayList<>();
+        for (DocumentationDto documentation : documentationsDto) {
+            Document document = new Document();
+            document.setDocument(String.join(" "," - ", documentation.getTypeDocument()
+                    , documentation.getNumberDocument()
+                    , documentation.getTitle()));
+            documentations.add(document);
+        }
+        return documentations;
+    }
+
+    private String getStringEmployees(EmployeeDto employee) {
+        String initials = String.join(". ", String.valueOf(employee.getName().charAt(0))
+                , String.valueOf(employee.getPatronymic().charAt(0)));
+        return String.join(" - ", " ", employee.getPost()
+                , String.join(", "
+                        , String.join(" ", employee.getSurname(), initials)
+                        , toStringCertificateEmployee(employee.getCertificate())));
+    }
+
+    private String setLaboratoryData(BranchDto branchDto, String license) {
+        return String.join(" ", branchDto.getDivision()
+                                      , String.join(" ", branchDto.getBranch()
+                                                               , toStringType(branchDto.getOrganization().getType())
+                                                               , branchDto.getOrganization().getName()
+                                                               , branchDto.getDivision())
+                                      , String.valueOf(branchDto.getIndex())
+                                      , branchDto.getAddress().getCity().getName().split(" ")[1]
+                                      , branchDto.getAddress().getStreet()
+                                      , stringService.getStringAddress(branchDto.getAddress())
+                                      , license);
+    }
+
+    private String toStringType(String type) {
+        String[] typeString = type.split(" ");
+        for (int i = 0; i < typeString.length; i++) {
+            typeString[i] = String.valueOf(typeString[i].charAt(0)).toUpperCase();
+        }
+        return Arrays.toString(typeString);
+    }
+
+    private String toStringCertificateEmployee(List<CertificateDto> certificateDto) {
+        List<Integer> levels = certificateDto.stream()
+                                             .map(CertificateDto::getLevel)
+                                             .distinct()
+                                             .toList();
+        List<String> organizations = certificateDto.stream()
+                                                   .map(CertificateDto::getOrganization)
+                                                   .map(OrganizationDto::getName)
+                                                   .distinct()
+                                                   .toList();
+        List<String> numbers = certificateDto.stream()
+                                             .map(CertificateDto::getNumber)
+                                             .distinct()
+                                             .toList();
+        if (levels.size() == 1 && organizations.size() == 1 && numbers.size() == 1) {
+            OrganizationDto organization = certificateDto.stream()
+                                                    .map(CertificateDto::getOrganization)
+                                                    .distinct()
+                                                    .toList().get(0);
+            String documentType = certificateDto.stream()
+                                                .map(CertificateDto::getDocumentType)
+                                                .distinct()
+                                                .toList().get(0);
+            return String.join(" ", String.valueOf(levels.get(0))
+                    , LEVEL_TEXT, String.join(",", certificateDto.stream()
+                                                                         .map(CertificateDto::getType)
+                                                                         .map(ControlTypeDto::getName)
+                                                                         .toList()).toUpperCase()
+                    , documentType, String.join(" ", String.valueOf(certificateDto.stream()
+                                                                                         .map(CertificateDto::getNumber)
+                                                                                         .distinct()
+                                                                                         .toList())
+                    , FROM, String.valueOf(certificateDto.stream()
+                                                          .map(CertificateDto::getStart)
+                                                          .distinct()
+                                                          .toList())
+                    , ISSUED, organization.getType().toUpperCase(), organization.getName())
+            );
+        } else {
+            List<String> certificates = new ArrayList<>();
+            for (CertificateDto certificate : certificateDto) {
+                certificates.add(String.join(" ", String.valueOf(certificate.getLevel())
+                        , LEVEL_TEXT, certificate.getType().getName().toUpperCase()
+                        , certificate.getDocumentType(), certificate.getNumber()
+                        , ISSUED, certificate.getOrganization().getType(), certificate.getOrganization().getName()));
+            }
+            return String.join("; ", certificates);
+        }
     }
 }
