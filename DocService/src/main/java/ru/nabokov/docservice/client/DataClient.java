e98f9f5ce98d215153c550ce895168c126872587
NabokovSG <nabokovsg@yandex.ru>
2023-05-12 15:54:00 +0300

    add method getApplication()

--- DocService/src/main/java/ru/nabokov/docservice/client/DataClient.java
+++ DocService/src/main/java/ru/nabokov/docservice/client/DataClient.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
-import ru.nabokov.docservice.dto.ApplicationDto;
+import ru.nabokov.docservice.dto.pattern.ApplicationDto;
 import ru.nabokov.docservice.dto.title.BranchDto;
 import java.util.Objects;
 
