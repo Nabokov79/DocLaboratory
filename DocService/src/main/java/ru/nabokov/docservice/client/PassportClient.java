da336715cc0b2a784525334d93f5785a5baafc9f
NabokovSG <nabokovsg@yandex.ru>
2023-05-12 16:05:44 +0300

    add client

---
+++ DocService/src/main/java/ru/nabokov/docservice/client/PassportClient.java
@@ -0,0 +1,28 @@
+package ru.nabokov.docservice.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import ru.nabokov.docservice.dto.passport.PassportDto;
+import java.util.Objects;
+
+@Component
+public class PassportClient {
+
+    private final WebClient webClient;
+
+    @Autowired
+    public PassportClient(@Qualifier("webPassportClient") WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public PassportDto getPassport(String uri) {
+        return Objects.requireNonNull(webClient.get()
+                        .uri(uri)
+                        .retrieve()
+                        .toEntity(PassportDto.class)
+                        .block())
+                .getBody();
+    }
+}
