99513313ed7377fce0258a3a49466184bc3d082e
NabokovSG <nabokovsg@yandex.ru>
2023-05-12 16:02:24 +0300

    without changes

--- DocService/src/main/java/ru/nabokov/docservice/mapper/TitleMapper.java
+++ DocService/src/main/java/ru/nabokov/docservice/mapper/TitleMapper.java
@@ -1,8 +1,8 @@
 package ru.nabokov.docservice.mapper;
 
 import org.mapstruct.Mapper;
-import ru.nabokov.docservice.dto.title.OrganizationDto;
-import ru.nabokov.docservice.dto.TextCenteredDto;
+import ru.nabokov.docservice.dto.OrganizationDto;
+import ru.nabokov.docservice.dto.pattern.TextCenteredDto;
 import ru.nabokov.docservice.model.Footer;
 import ru.nabokov.docservice.model.TextCentered;
 import ru.nabokov.docservice.model.TitleHeader;
