bde84fc1fd3c1f5dd3c628ab6e8d5ba19d788819
Nabokov79 <nabokovsg@yandex.ru>
2023-05-14 19:03:27 +0300

    update method save

--- DocService/src/main/java/ru/nabokov/docservice/service/ThirdSectionServiceImpl.java
+++ DocService/src/main/java/ru/nabokov/docservice/service/ThirdSectionServiceImpl.java
@@ -5,10 +5,8 @@ import org.springframework.stereotype.Service;
 import ru.nabokov.docservice.dto.passport.PassportDto;
 import ru.nabokov.docservice.dto.pattern.PatternSectionThreeDto;
 import ru.nabokov.docservice.dto.pattern.SubheadingThreeDto;
-import ru.nabokov.docservice.mapper.SectionMapper;
-import ru.nabokov.docservice.model.RepairsTable;
-import ru.nabokov.docservice.model.SurveysTable;
-import ru.nabokov.docservice.model.ThirdSection;
+import ru.nabokov.docservice.exceptions.BadRequestException;
+import ru.nabokov.docservice.model.*;
 import ru.nabokov.docservice.repository.ThirdSectionRepository;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -18,16 +16,36 @@ import java.util.stream.Collectors;
 public class ThirdSectionServiceImpl implements ThirdSectionService {
 
     private final ThirdSectionRepository repository;
-    private final SectionMapper sectionMapper;
+    private final RepairsTableService repairsTableService;
+    private final SurveysTableService surveysTableService;
+    private final StringBuilderService stringBuilder;
+
 
     @Override
     public ThirdSection save(PatternSectionThreeDto patternSectionThree, PassportDto passport) {
-        ThirdSection section = new ThirdSection();
         Map<Double, SubheadingThreeDto> subheadings = patternSectionThree.getSubheadings()
-                                             .stream().collect(Collectors.toMap(SubheadingThreeDto::getNumber, s -> s));
-        RepairsTable repairsTable = new RepairsTable();
-        SurveysTable surveysTable = new SurveysTable();
-        Double number;
-        return null;
+                .stream().collect(Collectors.toMap(SubheadingThreeDto::getNumber, s -> s));
+        if (subheadings.size() > 2) {
+            throw new BadRequestException(
+                    String.format("Section three cannot have more than two subsections, subsections=%s"
+                            , subheadings.size()));
+        }
+        ThirdSection section = new ThirdSection();
+        section.setHeading(stringBuilder.toStringHeader(patternSectionThree.getHeader()));
+        Double number = null;
+        for (Double numbers : subheadings.keySet()) {
+            if (number == null) {
+                number = numbers;
+            } else {
+                if (number > numbers) {
+                    section.setRepairsTable(repairsTableService.save(subheadings.get(number), passport.getRepairs()));
+                    section.setSurveysTable(surveysTableService.save(subheadings.get(numbers), passport.getSurveys()));
+                } else {
+                    section.setRepairsTable(repairsTableService.save(subheadings.get(numbers), passport.getRepairs()));
+                    section.setSurveysTable(surveysTableService.save(subheadings.get(number), passport.getSurveys()));
+                }
+            }
+        }
+        return repository.save(section);
     }
 }
