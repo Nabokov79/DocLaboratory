e1ae9ff5579018607b65dbb36be3dffb6f82af2c
NabokovSG <nabokovsg@yandex.ru>
2023-05-19 14:53:43 +0300

    add method addSectionToReport()

--- DocService/src/main/java/ru/nabokov/docservice/service/SeventhSectionServiceImpl.java
+++ DocService/src/main/java/ru/nabokov/docservice/service/SeventhSectionServiceImpl.java
@@ -5,7 +5,8 @@ import org.springframework.stereotype.Service;
 import ru.nabokov.docservice.dto.NewSeventhSectionDto;
 import ru.nabokov.docservice.dto.SeventhSectionDto;
 import ru.nabokov.docservice.mapper.SeventhSectionMapper;
-import ru.nabokov.docservice.model.SeventhSection;
+import ru.nabokov.docservice.model.Report;
+import ru.nabokov.docservice.model.seventhSection.SeventhSection;
 import ru.nabokov.docservice.repository.SeventhSectionRepository;
 
 @Service
@@ -16,6 +17,7 @@ public class SeventhSectionServiceImpl implements SeventhSectionService {
     private final SeventhSectionMapper mapper;
     private final StringBuilderService stringBuilder;
     private final DrawingService drawingService;
+    private final ReportService reportService;
 
     @Override
     public SeventhSectionDto save(NewSeventhSectionDto sectionDto) {
@@ -23,6 +25,13 @@ public class SeventhSectionServiceImpl implements SeventhSectionService {
         section.setHeading(stringBuilder.toStringHeader(mapper.mapToHeaderDto(sectionDto.getSectionHeaderDto())));
         section.setDrawings(drawingService.save(repository.save(section)
                                               , mapper.mapToDrawingDto(sectionDto.getDrawings())));
+        addSectionToReport(sectionDto.getReportId(), section);
         return mapper.mapToSeventhSectionDto(section);
+    }
+
+    private void addSectionToReport(Long reportId, SeventhSection section) {
+        Report report = reportService.get(reportId);
+        report.setSeventhSection(section);
+        reportService.update(report);
     }
 }
