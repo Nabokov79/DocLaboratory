75ec4035e6d4d1c8cdf8e566a14057ebcf2ebf91
NabokovSG <nabokovsg@yandex.ru>
2023-05-19 14:43:18 +0300

    without changes

--- DocService/src/main/java/ru/nabokov/docservice/service/ColumnsHeadersServiceImpl.java
+++ DocService/src/main/java/ru/nabokov/docservice/service/ColumnsHeadersServiceImpl.java
@@ -2,7 +2,7 @@ package ru.nabokov.docservice.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.nabokov.docservice.dto.pattern.ColumnHeaderDto;
+import ru.nabokov.docservice.dto.client.pattern.ColumnHeaderDto;
 import ru.nabokov.docservice.mapper.SectionMapper;
 import ru.nabokov.docservice.model.ColumnsHeaders;
 import ru.nabokov.docservice.repository.ColumnsHeadersRepository;
