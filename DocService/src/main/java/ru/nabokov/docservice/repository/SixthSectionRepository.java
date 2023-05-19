683ef386b78c73ac2c307b322e4bcb661c7b4dd9
NabokovSG <nabokovsg@yandex.ru>
2023-05-19 14:25:20 +0300

    add repository

---
+++ DocService/src/main/java/ru/nabokov/docservice/repository/SixthSectionRepository.java
@@ -0,0 +1,7 @@
+package ru.nabokov.docservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.nabokov.docservice.model.sixthSection.SixthSection;
+
+public interface SixthSectionRepository extends JpaRepository<SixthSection, Long> {
+}
