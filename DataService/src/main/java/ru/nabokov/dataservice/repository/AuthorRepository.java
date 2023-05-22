5279976cc99de8e1df6127a01d30295429533dd1
NabokovSG <nabokovsg@yandex.ru>
2023-05-22 10:45:13 +0300

    delete file

--- DataService/src/main/java/ru/nabokov/dataservice/repository/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.nabokov.dataservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.nabokov.dataservice.model.Author;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-
-    boolean existsByName(String name);
-}
