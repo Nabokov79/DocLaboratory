2a0e5d88992cec327b407bf544c9fc14e6b0bf79
NabokovSG <nabokovsg@yandex.ru>
2023-05-22 10:44:20 +0300

    update methods

--- DataService/src/main/java/ru/nabokov/dataservice/service/TypeServiceImpl.java
+++ DataService/src/main/java/ru/nabokov/dataservice/service/TypeServiceImpl.java
@@ -8,7 +8,9 @@ import ru.nabokov.dataservice.dto.type.UpdateTypeDto;
 import ru.nabokov.dataservice.exceptions.BadRequestException;
 import ru.nabokov.dataservice.exceptions.NotFoundException;
 import ru.nabokov.dataservice.mapper.TypeMapper;
+import ru.nabokov.dataservice.model.Documentation;
 import ru.nabokov.dataservice.model.Type;
+import ru.nabokov.dataservice.repository.DocumentationRepository;
 import ru.nabokov.dataservice.repository.TypeRepository;
 import java.util.List;
 
@@ -18,14 +20,18 @@ public class TypeServiceImpl implements TypeService {
 
     private final TypeRepository repository;
     private final TypeMapper mapper;
+    private final DocumentationRepository documentationRepository;
 
     @Override
     public TypeDto save(NewTypeDto typeDto) {
         if (repository.existsByName(typeDto.getName())) {
             throw new BadRequestException(String.format("type=%s found.", typeDto.getName()));
         }
-        Type type = mapper.mapToNewType(typeDto);
-        return mapper.mapToTypeDto(repository.save(type));
+        Type type = repository.save(mapper.mapToNewType(typeDto));
+        if (!typeDto.getDocumentationIds().isEmpty()) {
+            updateDocumentation(type, typeDto.getDocumentationIds());
+        }
+        return mapper.mapToTypeDto(type);
     }
 
     @Override
@@ -34,6 +40,9 @@ public class TypeServiceImpl implements TypeService {
             throw new NotFoundException(String.format("type=%s not found for update.", typeDto.getName()));
         }
         Type type = mapper.mapToUpdateType(typeDto);
+        if (!typeDto.getDocumentationIds().isEmpty()) {
+            updateDocumentation(type, typeDto.getDocumentationIds());
+        }
         return mapper.mapToTypeDto(repository.save(type));
     }
 
@@ -46,5 +55,13 @@ public class TypeServiceImpl implements TypeService {
     @Override
     public List<TypeDto> getAll() {
         return mapper.mapToTypesDto(repository.findAll());
+    }
+
+    private void updateDocumentation(Type type, List<Long> documentationIds) {
+        List<Documentation> documentations = documentationRepository.findAllById(documentationIds);
+        for (Documentation documentation : documentations) {
+            documentation.setType(type);
+            documentationRepository.saveAll(documentations);
+        }
     }
 }
