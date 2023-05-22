13e31d0ee388c077b672f44f73e0292258c249db
NabokovSG <nabokovsg@yandex.ru>
2023-05-22 11:11:46 +0300

    delete file

--- PatternService/src/main/java/ru/nabokov/patternservice/repository/DocumentationRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.nabokov.patternservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.nabokov.patternservice.model.Documentation;
-
-public interface DocumentationRepository extends JpaRepository<Documentation, Long> {
-}
