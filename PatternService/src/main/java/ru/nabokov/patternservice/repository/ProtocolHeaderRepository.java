62488d230e7d5b385620b26870ac552faac4dcf4
NabokovSG <nabokovsg@yandex.ru>
2023-05-26 14:14:06 +0300

    add method existsByNumber

--- PatternService/src/main/java/ru/nabokov/patternservice/repository/ProtocolHeaderRepository.java
+++ PatternService/src/main/java/ru/nabokov/patternservice/repository/ProtocolHeaderRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.nabokov.patternservice.model.ProtocolHeader;
 
 public interface ProtocolHeaderRepository extends JpaRepository<ProtocolHeader, Long> {
+
+    boolean existsByNumber(Integer number);
 }
