741fdecdacc3707251e5ccddafd2c719732205de
NabokovSG <nabokovsg@yandex.ru>
2023-05-26 14:03:26 +0300

    replace response type

--- PatternService/src/main/java/ru/nabokov/patternservice/client/DataClient.java
+++ PatternService/src/main/java/ru/nabokov/patternservice/client/DataClient.java
@@ -3,8 +3,7 @@ package ru.nabokov.patternservice.client;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
-import ru.nabokov.patternservice.dto.client.BranchDto;
-import ru.nabokov.patternservice.dto.client.Type;
+import ru.nabokov.patternservice.dto.client.TypeDto;
 
 import java.util.List;
 import java.util.Objects;
@@ -15,14 +14,14 @@ public class DataClient {
 
     private final WebClient webClient;
 
-    public List<Type> getType(String path, String ids) {
+    public List<TypeDto> getType(String path, String ids) {
         return Objects.requireNonNull(webClient.get()
                         .uri(uriBuilder -> uriBuilder
                                 .path(path)
                                 .queryParam("ids", ids)
                                 .build())
                         .retrieve()
-                        .toEntityList(Type.class)
+                        .toEntityList(TypeDto.class)
                         .block())
                 .getBody();
     }
