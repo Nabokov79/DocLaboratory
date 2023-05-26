87f25759301aff0ab8d5da5eb86b4149062645fa
NabokovSG <nabokovsg@yandex.ru>
2023-05-26 14:33:38 +0300

    update type field header

--- PatternService/src/main/java/ru/nabokov/patternservice/dto/section/UpdatePatternSectionDto.java
+++ PatternService/src/main/java/ru/nabokov/patternservice/dto/section/UpdatePatternSectionDto.java
@@ -4,9 +4,10 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import ru.nabokov.patternservice.dto.drawing.UpdateDrawingDto;
+import ru.nabokov.patternservice.dto.header.UpdateHeaderDto;
 import ru.nabokov.patternservice.dto.subheading.UpdateSubheadingDto;
-import ru.nabokov.patternservice.model.Header;
-import javax.validation.constraints.NotEmpty;
+
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import java.util.List;
@@ -15,7 +16,7 @@ import java.util.List;
 @Getter
 @AllArgsConstructor
 @Schema(description = "Данные для изменения информации в разделе № 1 шаблона отчета")
-public class UpdatePatternSectionOneDto {
+public class UpdatePatternSectionDto {
 
     @Schema(description = "Индентификатор")
     @NotNull(message = "pattern section one id should not be blank")
@@ -27,8 +28,9 @@ public class UpdatePatternSectionOneDto {
     private Long reportPatternId;
     @Schema(description = "Заголовок раздела")
     @NotNull(message = "header template of the first section should not be blank")
-    private Header header;
+    private UpdateHeaderDto header;
     @Schema(description = "Список подзаголовоков раздела")
-    @NotEmpty(message = "list of subheadings of the first section cannot be empty")
     private List<UpdateSubheadingDto> subheadings;
+    @Schema(description = "Список чертежей")
+    private List<UpdateDrawingDto> drawings;
 }
