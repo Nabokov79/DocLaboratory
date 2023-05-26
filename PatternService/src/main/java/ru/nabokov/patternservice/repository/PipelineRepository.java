af090b4222eeff99c9083c7da66571153f279e72
NabokovSG <nabokovsg@yandex.ru>
2023-05-26 14:40:31 +0300

    delete file

--- PatternService/src/main/java/ru/nabokov/patternservice/repository/PipelineRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.nabokov.patternservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.nabokov.patternservice.model.Pipeline;
-
-public interface PipelineRepository extends JpaRepository<Pipeline, Long> {
-}
--- PatternService/src/main/java/ru/nabokov/patternservice/repository/PipelineTableRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.nabokov.patternservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.nabokov.patternservice.model.PipelineTable;
-
-public interface PipelineTableRepository extends JpaRepository<PipelineTable, Long> {
-}
