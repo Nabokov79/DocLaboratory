718d495d8b5d6214c85c36dffab6f17b5f7f8b75
NabokovSG <nabokovsg@yandex.ru>
2023-05-26 14:33:05 +0300

    delete methods mapToPipelineProtocolPatternDto, mapToTankProtocolPatternDto

--- PatternService/src/main/java/ru/nabokov/patternservice/mapper/UltrasonicProtocolPatternMapper.java
+++ PatternService/src/main/java/ru/nabokov/patternservice/mapper/UltrasonicProtocolPatternMapper.java
@@ -2,8 +2,6 @@ package ru.nabokov.patternservice.mapper;
 
 import org.mapstruct.Mapper;
 import ru.nabokov.patternservice.dto.protocol.NewProtocolPatternDto;
-import ru.nabokov.patternservice.dto.PipelineProtocolPatternDto;
-import ru.nabokov.patternservice.dto.protocol.TankProtocolPatternDto;
 import ru.nabokov.patternservice.dto.protocol.UpdateProtocolPatternDto;
 import ru.nabokov.patternservice.model.UltrasonicProtocolPattern;
 
@@ -13,8 +11,4 @@ public interface UltrasonicProtocolPatternMapper {
     UltrasonicProtocolPattern mapToNewUltrasonicProtocolPattern(NewProtocolPatternDto patternDto);
 
     UltrasonicProtocolPattern mapToUpdateUltrasonicProtocolPattern(UpdateProtocolPatternDto patternDto);
-
-    PipelineProtocolPatternDto mapToPipelineProtocolPatternDto(UltrasonicProtocolPattern pattern);
-
-    TankProtocolPatternDto mapToTankProtocolPatternDto(UltrasonicProtocolPattern pattern);
 }
