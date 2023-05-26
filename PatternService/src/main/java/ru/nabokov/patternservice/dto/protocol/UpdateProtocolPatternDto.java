ecab0675b09dca3497211587fe86ddec63c0dbb8
NabokovSG <nabokovsg@yandex.ru>
2023-05-26 14:34:25 +0300

    update type fields

--- PatternService/src/main/java/ru/nabokov/patternservice/dto/protocol/UpdateProtocolPatternDto.java
+++ PatternService/src/main/java/ru/nabokov/patternservice/dto/protocol/UpdateProtocolPatternDto.java
@@ -4,11 +4,12 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import ru.nabokov.patternservice.model.PatternConclusion;
-import ru.nabokov.patternservice.model.PatternTable;
-import ru.nabokov.patternservice.model.ProtocolHeader;
+import ru.nabokov.patternservice.dto.conclusion.UpdatePatternConclusionDto;
+import ru.nabokov.patternservice.dto.protocolHeader.UpdateProtocolHeaderDto;
+import ru.nabokov.patternservice.dto.table.UpdatePatternTableDto;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
+import java.util.List;
 
 @Setter
 @Getter
@@ -21,16 +22,20 @@ public class UpdateProtocolPatternDto {
     @Positive(message = "id can only be positive")
     private Long id;
     @Schema(description = "Индентификатор шаблона отчета")
+    @NotNull(message = "pattern report id should not be blank")
+    @Positive(message = "pattern report id can only be positive")
+    private Long reportPatternId;
+    @Schema(description = "Индентификатор шаблона отчета")
     @NotNull(message = "pattern section id should not be blank")
     @Positive(message = "pattern section id can only be positive")
-    private Long sectionPatternId;
+    private Long sectionId;
     @Schema(description = "Заголовок протокола")
     @NotNull(message = "protocol header should not be blank")
-    private ProtocolHeader protocolHeader;
+    private UpdateProtocolHeaderDto protocolHeader;
     @Schema(description = "Таблица протокола")
     @NotNull(message = "protocol table should not be blank")
-    private PatternTable patternTable;
+    private List<UpdatePatternTableDto> patternTables;
     @Schema(description = "Заключение протокола")
     @NotNull(message = "protocol conclusion should not be blank")
-    private PatternConclusion patternConclusion;
+    private UpdatePatternConclusionDto patternConclusion;
 }
