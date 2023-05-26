964e4341e97d9beb47a2362e78391ac1949035e9
NabokovSG <nabokovsg@yandex.ru>
2023-05-26 14:07:24 +0300

    replace type - ColumnHeader -> NewColumnHeaderDto, CombinedColumns -> NewCombinedColumnsDto, TankElement -> NewElementDtoDto

--- PatternService/src/main/java/ru/nabokov/patternservice/dto/table/NewPatternTableDto.java
+++ PatternService/src/main/java/ru/nabokov/patternservice/dto/table/NewPatternTableDto.java
@@ -3,16 +3,19 @@ package ru.nabokov.patternservice.dto.table;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-import ru.nabokov.patternservice.dto.element.TankElement;
-import ru.nabokov.patternservice.model.ColumnHeader;
-import ru.nabokov.patternservice.model.CombinedColumns;
+import ru.nabokov.patternservice.dto.columnHeader.NewColumnHeaderDto;
+import ru.nabokov.patternservice.dto.combinedColumns.NewCombinedColumnsDto;
+import ru.nabokov.patternservice.dto.element.NewElementDto;
+
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Setter
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @Schema(description = "Данные нового шаблона таблицы")
 public class NewPatternTableDto {
 
@@ -20,9 +23,9 @@ public class NewPatternTableDto {
     private String name;
     @Schema(description = "Заголовки колонок таблицы")
     @NotNull(message = "pattern table column headers should not be blank")
-    private List<ColumnHeader> columnHeaders;
+    private List<NewColumnHeaderDto> columnHeaders;
     @Schema(description = "Данные объедененной колонки")
-    private CombinedColumns combinedColumn;
+    private NewCombinedColumnsDto combinedColumn;
     @Schema(description = "Элементы, содержащиеся в таблице")
-    private List<TankElement> elements;
+    private List<NewElementDto> elements;
 }
