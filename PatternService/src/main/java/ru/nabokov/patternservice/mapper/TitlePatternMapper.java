57565f2570e7f2911e5f389fad64c623305cbd37
NabokovSG <nabokovsg@yandex.ru>
2023-05-26 14:31:34 +0300

    without changes

--- PatternService/src/main/java/ru/nabokov/patternservice/mapper/TitlePatternMapper.java
+++ PatternService/src/main/java/ru/nabokov/patternservice/mapper/TitlePatternMapper.java
@@ -1,15 +1,14 @@
 package ru.nabokov.patternservice.mapper;
 
 import org.mapstruct.Mapper;
-import ru.nabokov.patternservice.dto.NewTitlePageDataDto;
-import ru.nabokov.patternservice.dto.TitlePatternDto;
-import ru.nabokov.patternservice.model.TitlePageData;
+import ru.nabokov.patternservice.dto.title.NewTitlePageDataDto;
+import ru.nabokov.patternservice.dto.title.UpdateTitlePageDataDto;
 import ru.nabokov.patternservice.model.TitlePattern;
 
 @Mapper(componentModel = "spring")
 public interface TitlePatternMapper {
 
-    TitlePageData mapToTextCentered(NewTitlePageDataDto reportDto);
+    TitlePattern mapToNewTitlePageData(NewTitlePageDataDto titlePageData);
 
-    TitlePatternDto mapToTitlePatternDto(TitlePattern report);
+    TitlePattern mapToUpdateTitlePageData(UpdateTitlePageDataDto titlePageData);
 }
