9f03f4557db8d3d8337ffbd573e7070a6e428683
NabokovSG <nabokovsg@yandex.ru>
2023-05-26 13:55:12 +0300

    replace parameters, update methods

--- PatternService/src/main/java/ru/nabokov/patternservice/service/protocol/DefectServiceImpl.java
+++ PatternService/src/main/java/ru/nabokov/patternservice/service/protocol/DefectServiceImpl.java
@@ -1,10 +1,13 @@
-package ru.nabokov.patternservice.service;
+package ru.nabokov.patternservice.service.protocol;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.nabokov.patternservice.exceptions.BadRequestException;
+import ru.nabokov.patternservice.dto.defect.NewDefectDto;
+import ru.nabokov.patternservice.dto.defect.UpdateDefectDto;
 import ru.nabokov.patternservice.exceptions.NotFoundException;
+import ru.nabokov.patternservice.mapper.DefectMapper;
 import ru.nabokov.patternservice.model.Defect;
+import ru.nabokov.patternservice.model.Element;
 import ru.nabokov.patternservice.repository.DefectRepository;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,25 +19,19 @@ import java.util.stream.Collectors;
 public class DefectServiceImpl implements DefectService {
 
     private final DefectRepository repository;
+    private final DefectMapper mapper;
 
     @Override
-    public void save(List<Defect> defects) {
-        validate(defects);
-        repository.saveAll(defects);
+    public void save(Element element, List<NewDefectDto> defectsDto) {
+        List<Defect> defects = mapper.mapToNewDefects(defectsDto);
+        repository.saveAll(setElement(element, defects));
     }
 
     @Override
-    public void update(List<Defect> defects) {
-        validateIds(defects.stream().map(Defect::getId).toList());
-        repository.saveAll(defects);
-    }
-
-    private void validate(List<Defect> defects) {
-        for (Defect defect : defects) {
-            if (defect.getName() == null) {
-                throw new BadRequestException("defect name cannot be blank");
-            }
-        }
+    public void update(Element element, List<UpdateDefectDto> defectsDto) {
+        validateIds(defectsDto.stream().map(UpdateDefectDto::getId).toList());
+        List<Defect> defects = mapper.mapToUpdateDefects(defectsDto);
+        repository.saveAll(setElement(element, defects));
     }
 
     private void validateIds(List<Long> ids) {
@@ -45,5 +42,12 @@ public class DefectServiceImpl implements DefectService {
             ids = ids.stream().filter(e -> !idsDb.contains(e)).collect(Collectors.toList());
             throw new NotFoundException(String.format("defects with ids= %s not found", ids));
         }
+    }
+
+    private List<Defect> setElement(Element element,  List<Defect> defects) {
+        for (Defect defect : defects) {
+            defect.setElement(element);
+        }
+        return defects;
     }
 }
