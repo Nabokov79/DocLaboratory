740ee2798dbb5697f5a6a8ac6d82a64a322b77bd
NabokovSG <nabokovsg@yandex.ru>
2023-05-26 13:55:47 +0300

    replace parameters

--- PatternService/src/main/java/ru/nabokov/patternservice/service/DrawingService.java
+++ PatternService/src/main/java/ru/nabokov/patternservice/service/DrawingService.java
@@ -1,12 +1,14 @@
 package ru.nabokov.patternservice.service;
 
+import ru.nabokov.patternservice.dto.drawing.NewDrawingDto;
+import ru.nabokov.patternservice.dto.drawing.UpdateDrawingDto;
 import ru.nabokov.patternservice.model.Drawing;
 import ru.nabokov.patternservice.model.PatternSectionSeven;
 import java.util.List;
 
 public interface DrawingService {
 
-    void save(PatternSectionSeven pattern, List<Drawing> drawings);
+    List<Drawing> save(PatternSectionSeven section, List<NewDrawingDto> drawingsDto);
 
-    void update(PatternSectionSeven pattern, List<Drawing> drawings);
+    List<Drawing> update(PatternSectionSeven section, List<UpdateDrawingDto> drawingsDto);
 }
