1e06481174062030a39b5102bb47ec325da805ff
NabokovSG <nabokovsg@yandex.ru>
2023-05-26 14:36:10 +0300

    delete method mapToVisualProtocolPatternDto

--- PatternService/src/main/java/ru/nabokov/patternservice/mapper/VisualProtocolPatternMapper.java
+++ PatternService/src/main/java/ru/nabokov/patternservice/mapper/VisualProtocolPatternMapper.java
@@ -3,7 +3,6 @@ package ru.nabokov.patternservice.mapper;
 import org.mapstruct.Mapper;
 import ru.nabokov.patternservice.dto.protocol.NewProtocolPatternDto;
 import ru.nabokov.patternservice.dto.protocol.UpdateProtocolPatternDto;
-import ru.nabokov.patternservice.dto.protocol.VisualProtocolPatternDto;
 import ru.nabokov.patternservice.model.VisualProtocolPattern;
 
 @Mapper(componentModel = "spring")
@@ -12,6 +11,4 @@ public interface VisualProtocolPatternMapper {
     VisualProtocolPattern mapToNewVisualProtocolPattern(NewProtocolPatternDto patternDto);
 
     VisualProtocolPattern mapToUpdateVisualProtocolPattern(UpdateProtocolPatternDto patternDto);
-
-    VisualProtocolPatternDto mapToVisualProtocolPatternDto(VisualProtocolPattern pattern);
 }
