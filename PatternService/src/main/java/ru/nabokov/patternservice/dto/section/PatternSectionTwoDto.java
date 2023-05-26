2dad6044e9ff9adad7c52fa0561831e271ef4d09
NabokovSG <nabokovsg@yandex.ru>
2023-05-26 14:10:56 +0300

    replace type field

--- PatternService/src/main/java/ru/nabokov/patternservice/dto/section/PatternSectionTwoDto.java
+++ PatternService/src/main/java/ru/nabokov/patternservice/dto/section/PatternSectionTwoDto.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import ru.nabokov.patternservice.dto.subheading.SubheadingDto;
+import ru.nabokov.patternservice.dto.subheading.ShortSubheadingDto;
 import ru.nabokov.patternservice.model.Header;
 import java.util.List;
 
@@ -19,7 +19,7 @@ public class PatternSectionTwoDto {
     @Schema(description = "Заголовок раздела")
     private Header header;
     @Schema(description = "Данные подзаголовоков раздела")
-    private List<SubheadingDto> subheadings;
+    private List<ShortSubheadingDto> subheadings;
 
     @Override
     public String toString() {
