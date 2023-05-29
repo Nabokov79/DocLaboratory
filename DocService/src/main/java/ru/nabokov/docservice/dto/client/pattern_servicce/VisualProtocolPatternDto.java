25b8295de6e27afd354af519ee0bdc62285b896a
NabokovSG <nabokovsg@yandex.ru>
2023-05-29 16:47:51 +0300

    renamed field type VisualProtocolHeaderDto -> ProtocolHeaderDto

--- DocService/src/main/java/ru/nabokov/docservice/dto/client/pattern_servicce/VisualProtocolPatternDto.java
+++ DocService/src/main/java/ru/nabokov/docservice/dto/client/pattern_servicce/VisualProtocolPatternDto.java
@@ -1,16 +1,16 @@
-package ru.nabokov.docservice.dto.client.pattern;
+package ru.nabokov.docservice.dto.client.pattern_servicce;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
-@NoArgsConstructor
+@AllArgsConstructor
 public class VisualProtocolPatternDto {
 
     private Long id;
-    private VisualProtocolHeaderDto protocolHeader;
+    private ProtocolHeaderDto protocolHeader;
     private PatternTableDto patternTable;
     private PatternConclusionDto patternConclusion;
 
