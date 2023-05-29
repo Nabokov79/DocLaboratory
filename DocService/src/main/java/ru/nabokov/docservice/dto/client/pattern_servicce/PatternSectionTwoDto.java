f6b1cba2823305a17bb0644e877e524f3df6e389
NabokovSG <nabokovsg@yandex.ru>
2023-05-29 16:51:42 +0300

    add field - id, replace type SubheadingDto -> ShortSubheadingDto

--- DocService/src/main/java/ru/nabokov/docservice/dto/client/pattern_servicce/PatternSectionTwoDto.java
+++ DocService/src/main/java/ru/nabokov/docservice/dto/client/pattern_servicce/PatternSectionTwoDto.java
@@ -1,22 +1,24 @@
-package ru.nabokov.docservice.dto.client.pattern;
+package ru.nabokov.docservice.dto.client.pattern_servicce;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import java.util.List;
 
 @Setter
 @Getter
-@NoArgsConstructor
+@AllArgsConstructor
 public class PatternSectionTwoDto {
 
+    private Long id;
     private HeaderDto header;
-    private List<SubheadingDto> subheadings;
+    private List<ShortSubheadingDto> subheadings;
 
     @Override
     public String toString() {
         return "PatternSectionTwoDto{" +
-                "header=" + header +
+                "id=" + id +
+                ", header=" + header +
                 ", subheadings=" + subheadings +
                 '}';
     }
