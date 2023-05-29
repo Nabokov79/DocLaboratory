9cbd10e2192b9b609466ef1cf85370356a2d5ad0
NabokovSG <nabokovsg@yandex.ru>
2023-05-29 16:59:28 +0300

    replace field type Header -> HeaderDto

--- PatternService/src/main/java/ru/nabokov/patternservice/dto/section/PatternSectionThreeDto.java
+++ PatternService/src/main/java/ru/nabokov/patternservice/dto/section/PatternSectionThreeDto.java
@@ -3,22 +3,20 @@ package ru.nabokov.patternservice.dto.section;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
+import ru.nabokov.patternservice.dto.header.HeaderDto;
 import ru.nabokov.patternservice.dto.subheading.SubheadingSectionThreeDto;
-import ru.nabokov.patternservice.model.Header;
 import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
 @Setter
 @Getter
 @AllArgsConstructor
-@NoArgsConstructor
 @Schema(description = "Данные раздела № 3 шаблона отчета")
 public class PatternSectionThreeDto {
 
     @Schema(description = "Заголовок раздела")
-    private Header header;
+    private HeaderDto header;
     @Schema(description = "Список подзаголовоков раздела")
     @NotEmpty(message = "list of subheadings of the three section cannot be empty")
     private List<SubheadingSectionThreeDto> subheadings;
--- PatternService/src/main/java/ru/nabokov/patternservice/dto/section/PatternSectionTwoDto.java
+++ PatternService/src/main/java/ru/nabokov/patternservice/dto/section/PatternSectionTwoDto.java
@@ -4,8 +4,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import ru.nabokov.patternservice.dto.header.HeaderDto;
 import ru.nabokov.patternservice.dto.subheading.ShortSubheadingDto;
-import ru.nabokov.patternservice.model.Header;
 import java.util.List;
 
 @Setter
@@ -17,7 +17,7 @@ public class PatternSectionTwoDto {
     @Schema(description = "Индентификатор")
     private Long id;
     @Schema(description = "Заголовок раздела")
-    private Header header;
+    private HeaderDto header;
     @Schema(description = "Данные подзаголовоков раздела")
     private List<ShortSubheadingDto> subheadings;
 
