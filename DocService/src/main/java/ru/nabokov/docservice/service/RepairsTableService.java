fc133dd1e5a5c43281e221ab8b6bb3f432b9e1b5
NabokovSG <nabokovsg@yandex.ru>
2023-05-30 09:06:07 +0300

    replace field type SubheadingThreeDto -> SubheadingSectionThreeDto

--- DocService/src/main/java/ru/nabokov/docservice/service/RepairsTableService.java
+++ DocService/src/main/java/ru/nabokov/docservice/service/RepairsTableService.java
@@ -1,12 +1,12 @@
 package ru.nabokov.docservice.service;
 
-import ru.nabokov.docservice.dto.client.passport.RepairDto;
-import ru.nabokov.docservice.dto.client.pattern.SubheadingThreeDto;
+import ru.nabokov.docservice.dto.client.passport_service.RepairDto;
+import ru.nabokov.docservice.dto.client.pattern_servicce.SubheadingSectionThreeDto;
 import ru.nabokov.docservice.model.thirdSection.RepairsTable;
 
 import java.util.List;
 
 public interface RepairsTableService {
 
-    RepairsTable save(SubheadingThreeDto subheadingDto, List<RepairDto> repairs);
+    RepairsTable save(SubheadingSectionThreeDto subheadingDto, List<RepairDto> repairs);
 }
--- DocService/src/main/java/ru/nabokov/docservice/service/RepairsTableServiceImpl.java
+++ DocService/src/main/java/ru/nabokov/docservice/service/RepairsTableServiceImpl.java
@@ -2,8 +2,8 @@ package ru.nabokov.docservice.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.nabokov.docservice.dto.client.passport.RepairDto;
-import ru.nabokov.docservice.dto.client.pattern.SubheadingThreeDto;
+import ru.nabokov.docservice.dto.client.passport_service.RepairDto;
+import ru.nabokov.docservice.dto.client.pattern_servicce.SubheadingSectionThreeDto;
 import ru.nabokov.docservice.model.thirdSection.RepairsTable;
 import ru.nabokov.docservice.repository.RepairsTableRepository;
 
@@ -16,13 +16,13 @@ public class RepairsTableServiceImpl implements RepairsTableService {
     private final RepairsTableRepository repository;
     private final RepairsService repairsService;
     private final ColumnsHeadersService columnsHeadersService;
-    private final StringBuilderService stringBuilder;
+    private final StringBuilderServiceImpl stringBuilder;
 
     @Override
-    public RepairsTable save(SubheadingThreeDto subheadingDto, List<RepairDto> repairs) {
+    public RepairsTable save(SubheadingSectionThreeDto subheadingDto, List<RepairDto> repairs) {
         RepairsTable repairsTable = new RepairsTable();
         repairsTable.setHeading(stringBuilder.toStringSubheading(subheadingDto.getNumber(), subheadingDto.getHeading()));
-        repairsTable.setColumnHeaders(columnsHeadersService.save(subheadingDto.getColumnHeaders()));
+        repairsTable.setColumnHeaders(columnsHeadersService.save(subheadingDto.getPatternTables().getColumnHeaders()));
         repairsTable.setRepairs(repairsService.save(repository.save(repairsTable), repairs));
         return repairsTable;
     }
