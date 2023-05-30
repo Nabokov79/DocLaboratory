8fa3d4d51f8c4be6720c5310583f47e1dd89be1f
NabokovSG <nabokovsg@yandex.ru>
2023-05-30 09:10:00 +0300

    delete methods: mapToHeaderDto, mapToSeventhSectionDto

--- DocService/src/main/java/ru/nabokov/docservice/mapper/SeventhSectionMapper.java
+++ DocService/src/main/java/ru/nabokov/docservice/mapper/SeventhSectionMapper.java
@@ -2,20 +2,11 @@ package ru.nabokov.docservice.mapper;
 
 import org.mapstruct.Mapper;
 import ru.nabokov.docservice.dto.NewDrawingDto;
-import ru.nabokov.docservice.dto.SectionHeaderDto;
-import ru.nabokov.docservice.dto.SeventhSectionDto;
-import ru.nabokov.docservice.dto.client.pattern.DrawingDto;
-import ru.nabokov.docservice.dto.client.pattern.HeaderDto;
-import ru.nabokov.docservice.model.seventhSection.SeventhSection;
-
+import ru.nabokov.docservice.dto.client.pattern_servicce.DrawingDto;
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface SeventhSectionMapper {
 
-    HeaderDto mapToHeaderDto(SectionHeaderDto headerDto);
-
     List<DrawingDto> mapToDrawingDto(List<NewDrawingDto> drawings);
-
-    SeventhSectionDto mapToSeventhSectionDto(SeventhSection section);
 }
