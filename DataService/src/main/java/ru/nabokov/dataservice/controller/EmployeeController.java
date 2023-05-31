74f618267b6c15a5b197789e339abff9c3ec7099
NabokovSG <nabokovsg@yandex.ru>
2023-05-31 16:49:38 +0300

    update uri

--- DataService/src/main/java/ru/nabokov/dataservice/controller/EmployeeController.java
+++ DataService/src/main/java/ru/nabokov/dataservice/controller/EmployeeController.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(
-        value = "/data/users",
+        value = "/data/employee",
         consumes = MediaType.ALL_VALUE,
         produces = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
