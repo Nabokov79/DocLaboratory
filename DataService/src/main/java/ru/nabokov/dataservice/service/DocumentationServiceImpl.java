4c9c609448be22f637d4d94d368bbc904eddc997
NabokovSG <nabokovsg@yandex.ru>
2023-06-23 14:25:27 +0300

    update methods save

--- DataService/src/main/java/ru/nabokov/dataservice/service/DocumentationServiceImpl.java
+++ DataService/src/main/java/ru/nabokov/dataservice/service/DocumentationServiceImpl.java
@@ -5,12 +5,10 @@ import org.springframework.stereotype.Service;
 import ru.nabokov.dataservice.dto.documentation.DocumentationDto;
 import ru.nabokov.dataservice.dto.documentation.NewDocumentationDto;
 import ru.nabokov.dataservice.dto.documentation.UpdateDocumentationDto;
-import ru.nabokov.dataservice.exceptions.BadRequestException;
 import ru.nabokov.dataservice.exceptions.NotFoundException;
 import ru.nabokov.dataservice.mapper.DocumentationMapper;
 import ru.nabokov.dataservice.model.Documentation;
 import ru.nabokov.dataservice.repository.DocumentationRepository;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -26,8 +24,19 @@ public class DocumentationServiceImpl implements DocumentationService {
 
     @Override
     public List<DocumentationDto> save(List<NewDocumentationDto> documentationsDto) {
-        validateByTitle(documentationsDto.stream().map(NewDocumentationDto::getTitle).toList());
-        return mapper.mapToDocumentationDto(repository.saveAll(mapper.mapToNewDocumentation(documentationsDto)));
+        Map<String, Documentation> documentations = mapper.mapToNewDocumentation(documentationsDto)
+                                                   .stream().collect(Collectors.toMap(Documentation::getTitle, d -> d));
+        Set<Documentation> documentationsDb = repository.findAllByTitle(documentationsDto
+                                                                 .stream().map(NewDocumentationDto::getTitle).toList());
+        List<Documentation> documentationDb = new ArrayList<>();
+        if (documentationsDb != null && !documentationsDb.isEmpty()) {
+            for (Documentation documentation : documentationsDb) {
+                documentations.remove(documentation.getTitle());
+            }
+            documentationDb.addAll(documentationsDb);
+        }
+        documentationDb.addAll(repository.saveAll(new ArrayList<>(documentations.values())));
+        return mapper.mapToDocumentationDto(documentationDb);
     }
 
     @Override
@@ -48,13 +57,6 @@ public class DocumentationServiceImpl implements DocumentationService {
                 );
         repository.deleteById(id);
         return String.join(" ", documentation.getView(), documentation.getNumber());
-    }
-
-    private void validateByTitle(List<String> titles) {
-        Set<Documentation> documentations = repository.findAllByTitle(titles);
-        if (documentations.size() != titles.size() && !documentations.isEmpty()) {
-            throw new BadRequestException(String.format("documentations with title= %s found", titles));
-        }
     }
 
     private void validateIds(List<Long> ids) {
