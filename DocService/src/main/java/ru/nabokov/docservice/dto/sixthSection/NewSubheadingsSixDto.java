7cd279b4515acf823d68f25cf4cf48b46a3fd326
NabokovSG <nabokovsg@yandex.ru>
2023-06-23 14:59:56 +0300

    replace all fields

--- DocService/src/main/java/ru/nabokov/docservice/dto/sixthSection/NewSubheadingsSixDto.java
+++ DocService/src/main/java/ru/nabokov/docservice/dto/sixthSection/NewSubheadingsSixDto.java
@@ -5,9 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import java.util.List;
@@ -23,14 +20,8 @@ public class NewSubheadingsSixDto {
     @NotNull(message = "number should not be blank")
     @Positive(message = "number must be positive")
     private Double number;
-    @Schema(description = "Название подраздела")
-    @NotBlank(message = " heading should not be blank")
-    private String heading;
-    @Schema(description = "Заключение")
-    @NotBlank(message = "conclusion should not be blank")
-    private String conclusion;
+    @Schema(description = "Разрешенный срок эксплуатации")
+    private Integer year;
     @Schema(description = "Рекомендации")
-    @NotNull(message = "recommendations should not be blank")
-    @NotEmpty(message = "recommendations should not be empty")
     private List<NewRecommendationsDto> recommendations;
 }
