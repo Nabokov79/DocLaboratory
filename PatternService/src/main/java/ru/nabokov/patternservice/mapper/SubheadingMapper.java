aea3632ff90856e57def5dbcecedd5637c26244e
NabokovSG <nabokovsg@yandex.ru>
2023-06-23 14:35:47 +0300

    add method  mapToShortSubheadingDto

--- PatternService/src/main/java/ru/nabokov/patternservice/mapper/SubheadingMapper.java
+++ PatternService/src/main/java/ru/nabokov/patternservice/mapper/SubheadingMapper.java
@@ -2,6 +2,7 @@ package ru.nabokov.patternservice.mapper;
 
 import org.mapstruct.Mapper;
 import ru.nabokov.patternservice.dto.subheading.NewSubheadingDto;
+import ru.nabokov.patternservice.dto.subheading.ShortSubheadingDto;
 import ru.nabokov.patternservice.dto.subheading.UpdateSubheadingDto;
 import ru.nabokov.patternservice.model.Subheading;
 import java.util.List;
@@ -12,4 +13,6 @@ public interface SubheadingMapper {
     List<Subheading> mapToNewSubheadings(List<NewSubheadingDto> subheadings);
 
     List<Subheading> mapToUpdateSubheadings(List<UpdateSubheadingDto> subheadings);
+
+    List<ShortSubheadingDto> mapToShortSubheadingDto(List<Subheading> subheadings);
 }
