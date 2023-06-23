53d0bc993023ad45e36a75ce4a3b1f1de2e4ea14
NabokovSG <nabokovsg@yandex.ru>
2023-06-23 14:36:47 +0300

    delete methods: mapToUpdateSixthSection, mapToSixthSectionDto

--- DocService/src/main/java/ru/nabokov/docservice/mapper/SixthSectionMapper.java
+++ DocService/src/main/java/ru/nabokov/docservice/mapper/SixthSectionMapper.java
@@ -1,17 +1,11 @@
 package ru.nabokov.docservice.mapper;
 
 import org.mapstruct.Mapper;
-import ru.nabokov.docservice.dto.sixthSection.NewSixthSectionDto;
-import ru.nabokov.docservice.dto.sixthSection.SixthSectionDto;
-import ru.nabokov.docservice.dto.sixthSection.UpdateSixthSectionDto;
+import ru.nabokov.docservice.dto.client.pattern_servicce.PatternSectionSixDto;
 import ru.nabokov.docservice.model.sixthSection.SixthSection;
 
 @Mapper(componentModel = "spring")
 public interface SixthSectionMapper {
 
-    SixthSection mapToNewSixthSection(NewSixthSectionDto sectionDto);
-
-    SixthSection mapToUpdateSixthSection(UpdateSixthSectionDto sectionDto);
-
-    SixthSectionDto mapToSixthSectionDto(SixthSection section);
+    SixthSection mapToNewSixthSection(PatternSectionSixDto pattern);
 }
