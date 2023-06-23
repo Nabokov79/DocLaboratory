8a5e1216c5b4f33fb3a7d66d3bca5ddd88cead50
NabokovSG <nabokovsg@yandex.ru>
2023-06-23 14:37:08 +0300

    add method mapToShortReportPatternsDto

--- PatternService/src/main/java/ru/nabokov/patternservice/mapper/ReportPatternMapper.java
+++ PatternService/src/main/java/ru/nabokov/patternservice/mapper/ReportPatternMapper.java
@@ -2,10 +2,13 @@ package ru.nabokov.patternservice.mapper;
 
 import org.mapstruct.Mapper;
 import ru.nabokov.patternservice.dto.ReportPatternDto;
+import ru.nabokov.patternservice.dto.ShortReportPatternDto;
 import ru.nabokov.patternservice.model.ReportPattern;
 
 @Mapper(componentModel = "spring")
 public interface ReportPatternMapper {
 
     ReportPatternDto mapToReportPatternDto(ReportPattern pattern);
+
+    ShortReportPatternDto mapToShortReportPatternsDto(ReportPattern patterns);
 }
