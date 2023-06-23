9dd1f3aa4159dd9cbd3ac6f2c1a8273afaf18982
NabokovSG <nabokovsg@yandex.ru>
2023-06-23 14:24:36 +0300

    update methods: save, update

--- PassportService/src/main/java/ru/nabokov/passportservice/service/CharacteristicServiceImpl.java
+++ PassportService/src/main/java/ru/nabokov/passportservice/service/CharacteristicServiceImpl.java
@@ -2,6 +2,8 @@ package ru.nabokov.passportservice.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.nabokov.passportservice.client.PassportClient;
+import ru.nabokov.passportservice.dto.client.ShortSubheadingDto;
 import ru.nabokov.passportservice.exceptions.BadRequestException;
 import ru.nabokov.passportservice.model.Characteristic;
 import ru.nabokov.passportservice.dto.сharacteristic.NewCharacteristicDto;
@@ -21,15 +23,26 @@ public class CharacteristicServiceImpl implements CharacteristicService {
 
     private final CharacteristicRepository repository;
     private final CharacteristicMapper mapper;
+    private final PassportClient client;
 
     @Override
     public List<Characteristic> save(Passport passport, List<NewCharacteristicDto> characteristicsDto) {
         if (repository.existsAllByPassport(passport)) {
             throw new BadRequestException(String.format("Characteristic for passport=%s found", passport));
         }
-        List<Characteristic> characteristics = mapper.mapFromNewCharacteristics(characteristicsDto);
-        for (Characteristic characteristic : characteristics) {
+        Map<Long,ShortSubheadingDto> subheadings = getShortSubheadings(characteristicsDto
+                                                          .stream()
+                                                          .map(NewCharacteristicDto::getSubheadingId)
+                                                          .toList())
+                                                          .stream()
+                                                          .collect(Collectors.toMap(ShortSubheadingDto::getId, s -> s));
+        List<Characteristic> characteristics = new ArrayList<>();
+        for (NewCharacteristicDto characteristicDto : characteristicsDto) {
+            Characteristic characteristic = mapper.mapFromNewCharacteristics(characteristicDto);
+            characteristic.setNumber(subheadings.get(characteristicDto.getSubheadingId()).getNumber());
+            characteristic.setHeading(subheadings.get(characteristicDto.getSubheadingId()).getHeading());
             characteristic.setPassport(passport);
+            characteristics.add(characteristic);
         }
         return repository.saveAll(characteristics);
     }
@@ -37,9 +50,19 @@ public class CharacteristicServiceImpl implements CharacteristicService {
     @Override
     public List<Characteristic> update(Passport passport, List<UpdateCharacteristicDto> characteristicsDto) {
         validateIds(characteristicsDto.stream().map(UpdateCharacteristicDto::getId).toList());
-        List<Characteristic> characteristics = mapper.mapFromUpdateCharacteristics(characteristicsDto);
-        for (Characteristic characteristic : characteristics) {
+        Map<Long,ShortSubheadingDto> subheadings = getShortSubheadings(characteristicsDto
+                                                          .stream()
+                                                          .map(UpdateCharacteristicDto::getSubheadingId)
+                                                          .toList())
+                                                          .stream()
+                                                          .collect(Collectors.toMap(ShortSubheadingDto::getId, s -> s));
+        List<Characteristic> characteristics = new ArrayList<>();
+        for (UpdateCharacteristicDto characteristicDto : characteristicsDto) {
+            Characteristic characteristic = mapper.mapFromUpdateCharacteristics(characteristicDto);
+            characteristic.setNumber(subheadings.get(characteristicDto.getSubheadingId()).getNumber());
+            characteristic.setHeading(subheadings.get(characteristicDto.getSubheadingId()).getHeading());
             characteristic.setPassport(passport);
+            characteristics.add(characteristic);
         }
         return repository.saveAll(characteristics);
     }
@@ -52,5 +75,9 @@ public class CharacteristicServiceImpl implements CharacteristicService {
             ids = ids.stream().filter(e -> !idsDb.contains(e)).collect(Collectors.toList());
             throw new NotFoundException(String.format("characteristics with ids= %s not found", ids));
         }
+    }
+
+    private List<ShortSubheadingDto> getShortSubheadings(List<Long> ids) {
+        return client.getSubheadings(String.join(",", ids.stream().map(String::valueOf).toList()));
     }
 }
