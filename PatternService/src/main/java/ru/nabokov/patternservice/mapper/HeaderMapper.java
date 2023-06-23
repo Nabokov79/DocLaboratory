4a861e53739863c9ea8798d6ac22820b108f90f3
NabokovSG <nabokovsg@yandex.ru>
2023-06-23 14:26:50 +0300

    add methjd mapToHeadersDto

--- PatternService/src/main/java/ru/nabokov/patternservice/mapper/HeaderMapper.java
+++ PatternService/src/main/java/ru/nabokov/patternservice/mapper/HeaderMapper.java
@@ -1,9 +1,12 @@
 package ru.nabokov.patternservice.mapper;
 
 import org.mapstruct.Mapper;
+import ru.nabokov.patternservice.dto.header.HeaderDto;
 import ru.nabokov.patternservice.dto.header.NewHeaderDto;
 import ru.nabokov.patternservice.dto.header.UpdateHeaderDto;
 import ru.nabokov.patternservice.model.Header;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface HeaderMapper {
@@ -11,4 +14,6 @@ public interface HeaderMapper {
     Header mapToNewHeader(NewHeaderDto headerDto);
 
     Header mapToUpdateHeader(UpdateHeaderDto headerDto);
+
+    List<HeaderDto> mapToHeadersDto(List<Header> header);
 }
