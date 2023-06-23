62859e6d2953a64018b7db2023f1ee6b69c97a3e
NabokovSG <nabokovsg@yandex.ru>
2023-06-23 14:31:56 +0300

    delete method - update

--- DocService/src/main/java/ru/nabokov/docservice/service/SubheadingsService.java
+++ DocService/src/main/java/ru/nabokov/docservice/service/SubheadingsService.java
@@ -1,14 +1,12 @@
 package ru.nabokov.docservice.service;
 
 import ru.nabokov.docservice.dto.client.data_service.ObjectDataDto;
+import ru.nabokov.docservice.dto.client.pattern_servicce.SubheadingDto;
 import ru.nabokov.docservice.dto.sixthSection.NewSubheadingsSixDto;
-import ru.nabokov.docservice.dto.sixthSection.UpdateSubheadingsSixDto;
 import ru.nabokov.docservice.model.sixthSection.SixthSection;
 import java.util.List;
 
 public interface SubheadingsService {
 
-    void save(SixthSection section, ObjectDataDto objectData, List<NewSubheadingsSixDto> subheadingsDto);
-
-    void update(SixthSection section, ObjectDataDto objectData,List<UpdateSubheadingsSixDto> subheadingsDto);
+    void save( ObjectDataDto objectData, SixthSection section, List<SubheadingDto> subheadings, List<NewSubheadingsSixDto> subheadingsDto);
 }
--- DocService/src/main/java/ru/nabokov/docservice/service/SubheadingsServiceImpl.java
+++ DocService/src/main/java/ru/nabokov/docservice/service/SubheadingsServiceImpl.java
@@ -3,14 +3,16 @@ package ru.nabokov.docservice.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.nabokov.docservice.dto.client.data_service.ObjectDataDto;
+import ru.nabokov.docservice.dto.client.pattern_servicce.SubheadingDto;
 import ru.nabokov.docservice.dto.sixthSection.NewSubheadingsSixDto;
-import ru.nabokov.docservice.dto.sixthSection.UpdateSubheadingsSixDto;
 import ru.nabokov.docservice.model.sixthSection.SixthSection;
 import ru.nabokov.docservice.model.sixthSection.SubheadingSection;
 import ru.nabokov.docservice.repository.SubheadingsRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -21,24 +23,20 @@ public class SubheadingsServiceImpl implements SubheadingsService {
     private final StringBuilderServiceImpl stringBuilder;
 
     @Override
-    public void save(SixthSection section, ObjectDataDto objectData, List<NewSubheadingsSixDto> subheadingsDto) {
-        List<SubheadingSection> subheadings = new ArrayList<>();
+    public void save(ObjectDataDto objectData, SixthSection section, List<SubheadingDto> subheadings, List<NewSubheadingsSixDto> subheadingsDto) {
+        Map<Double,SubheadingDto> subheadingsMap = subheadings.stream().collect(Collectors.toMap(SubheadingDto::getNumber, s -> s));
+        List<SubheadingSection> subheadingsDb = new ArrayList<>();
         for (NewSubheadingsSixDto newSubheading : subheadingsDto) {
             SubheadingSection subheading = new SubheadingSection();
             subheading.setSubheading(stringBuilder.toStringSubheading(newSubheading.getNumber()
-                                                                    , newSubheading.getHeading()));
+                                                                    , subheadingsMap.get(newSubheading.getNumber()).getHeading()));
             subheading.setConclusion(stringBuilder.toStringConclusion(objectData.getType()
                                                                     , objectData.getNumber()
-                                                                    , newSubheading.getConclusion()));
+                                                                    , subheadingsMap.get(newSubheading.getNumber()).getConclusions().getIsNotDefect()));
             subheading.setRecommendations(recommendationsService.save(newSubheading.getRecommendations()));
             subheading.setSixthSection(section);
-            subheadings.add(subheading);
+            subheadingsDb.add(subheading);
         }
-        repository.saveAll(subheadings);
-    }
-
-    @Override
-    public void update(SixthSection section, ObjectDataDto objectData,  List<UpdateSubheadingsSixDto> subheadingsDto) {
-
+        repository.saveAll(subheadingsDb);
     }
 }
