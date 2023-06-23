62cd84331b32e13deab945e3d6330222beca55df
NabokovSG <nabokovsg@yandex.ru>
2023-06-23 14:41:23 +0300

    add methods for pattern-service

--- PassportService/src/main/java/ru/nabokov/passportservice/client/PassportClient.java
+++ PassportService/src/main/java/ru/nabokov/passportservice/client/PassportClient.java
@@ -4,19 +4,26 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.nabokov.passportservice.dto.client.ObjectDataDto;
 import ru.nabokov.passportservice.dto.client.OrganizationDto;
+import ru.nabokov.passportservice.dto.client.ShortSubheadingDto;
+
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PassportClient {
 
-    private final DataClient client;
+    private final DataClient dataClient;
+    private final PatternClient patternClient;
 
     public ObjectDataDto getObjectData(Long id) {
-        return client.getObjectData("/data/object/" + id);
+        return dataClient.getObjectData("/data/object/" + id);
     }
 
     public List<OrganizationDto> getOrganizations(String ids) {
-        return client.getOrganizations("/data/organizations/", ids);
+        return dataClient.getOrganizations("/data/organizations", ids);
+    }
+
+    public List<ShortSubheadingDto> getSubheadings(String ids) {
+        return patternClient.getSubheadings("/pattern/subheadings", ids);
     }
 }
