f54915ca4f52e02324dd3f7ef95270d4592326b5
NabokovSG <nabokovsg@yandex.ru>
2023-06-23 15:03:59 +0300

    delete fields: HeaderDto header

--- DocService/src/main/java/ru/nabokov/docservice/dto/sixthSection/UpdateSixthSectionDto.java
+++ DocService/src/main/java/ru/nabokov/docservice/dto/sixthSection/UpdateSixthSectionDto.java
@@ -4,8 +4,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import ru.nabokov.docservice.dto.client.pattern_servicce.HeaderDto;
-
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
@@ -24,9 +22,6 @@ public class UpdateSixthSectionDto {
     @NotNull(message = "report id should not be blank")
     @Positive(message = "report id can only be positive")
     private Long reportId;
-    @Schema(description = "Заголовок раздела")
-    @NotNull(message = "header should not be blank")
-    private HeaderDto header;
     @Schema(description = "Данные подразделов")
     @NotNull(message = "subheadings should not be blank")
     @NotEmpty(message = "subheadings should not be empty")
