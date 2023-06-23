f8b2990700a37fa7936f9e0f717b5ca825c85963
NabokovSG <nabokovsg@yandex.ru>
2023-06-23 14:32:49 +0300

    update methods - update, save

--- PatternService/src/main/java/ru/nabokov/patternservice/service/section/TitlePatternServiceImpl.java
+++ PatternService/src/main/java/ru/nabokov/patternservice/service/section/TitlePatternServiceImpl.java
@@ -8,6 +8,7 @@ import ru.nabokov.patternservice.dto.title.UpdateTitlePageDataDto;
 import ru.nabokov.patternservice.exceptions.BadRequestException;
 import ru.nabokov.patternservice.exceptions.NotFoundException;
 import ru.nabokov.patternservice.mapper.TitlePatternMapper;
+import ru.nabokov.patternservice.model.ReportPattern;
 import ru.nabokov.patternservice.repository.TitlePatternRepository;
 import ru.nabokov.patternservice.service.ReportPatternService;
 
@@ -18,26 +19,30 @@ public class TitlePatternServiceImpl implements TitlePatternService {
     private final TitlePatternRepository repository;
     private final TitlePatternMapper mapper;
     private final ReportPatternService reportPatternService;
+    private final ReportPatternForSectionsService sectionsService;
 
     @Override
     public ReportPatternDto save(NewTitlePageDataDto titlePageDataDto) {
+        ReportPattern pattern = new ReportPattern();
         if (repository.existsByTypeId(titlePageDataDto.getTypeId())) {
             throw new BadRequestException(
                     String.format("title page data for typeId=%s found", titlePageDataDto.getTypeId())
             );
         }
-        return reportPatternService.addTitlePattern(titlePageDataDto.getTypeId()
-                                       , repository.save(mapper.mapToNewTitlePageData(titlePageDataDto)));
+        pattern.setTypeId(titlePageDataDto.getTypeId());
+        pattern.setTitlePattern(repository.save(mapper.mapToNewTitlePageData(titlePageDataDto)));
+        return reportPatternService.save(pattern);
     }
 
     @Override
     public ReportPatternDto update(UpdateTitlePageDataDto  titlePageDataDto) {
+        ReportPattern pattern = sectionsService.get(titlePageDataDto.getReportPatternId());
         if (!repository.existsById(titlePageDataDto.getId())) {
             throw new NotFoundException(
                              String.format("Report pattern witch id=%s not found for update",  titlePageDataDto.getId())
             );
         }
-        return  reportPatternService.addTitlePattern(titlePageDataDto.getTypeId()
-                                                  , repository.save(mapper.mapToUpdateTitlePageData(titlePageDataDto)));
+        pattern.setTitlePattern(repository.save(mapper.mapToUpdateTitlePageData(titlePageDataDto)));
+        return reportPatternService.save(pattern);
     }
 }
