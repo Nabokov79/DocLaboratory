2cae7c2a6aa456fc9d5101fefbe30b68135927f5
Nabokov79 <nabokovsg@yandex.ru>
2023-07-03 22:01:50 +0300

    add controller class

---
+++ DataService/src/main/java/ru/nabokov/dataservice/controller/ElementController.java
@@ -0,0 +1,44 @@
+package ru.nabokov.dataservice.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.nabokov.dataservice.dto.element.ElementDto;
+import ru.nabokov.dataservice.dto.element.NewElementDto;
+import ru.nabokov.dataservice.dto.element.UpdateElementDto;
+import ru.nabokov.dataservice.service.ElementService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(
+        value = "/data/elements",
+        consumes = MediaType.ALL_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE)
+@RequiredArgsConstructor
+@Validated
+@Tag(name="Элемент объекта",
+        description="API для работы с данными елемента объекта")
+public class ElementController {
+
+    private final ElementService service;
+
+    @Operation(summary = "Добавление нового нормативного документа")
+    @PostMapping
+    public ResponseEntity<List<ElementDto>> save(
+            @RequestBody @Parameter(description = "Список элементов") List<NewElementDto> elementsDto) {
+        return ResponseEntity.ok().body(service.save(elementsDto));
+    }
+
+    @Operation(summary = "Изменение данных нормативного документа")
+    @PatchMapping
+    public ResponseEntity<List<ElementDto>> update(
+            @RequestBody @Parameter(description = "Список элементов") List<UpdateElementDto> elementsDto) {
+        return ResponseEntity.ok().body(service.update(elementsDto));
+    }
+}
