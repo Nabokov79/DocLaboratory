1be840e0367c9c934d38060387548c48011ec5ac
NabokovSG <nabokovsg@yandex.ru>
2023-07-11 10:27:35 +0300

    update methods: save update

--- DataService/src/main/java/ru/nabokov/dataservice/service/ElementServiceImpl.java
+++ DataService/src/main/java/ru/nabokov/dataservice/service/ElementServiceImpl.java
@@ -1,7 +1,6 @@
 package ru.nabokov.dataservice.service;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.nabokov.dataservice.dto.element.ElementDto;
 import ru.nabokov.dataservice.dto.element.NewElementDto;
@@ -18,50 +17,25 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class ElementServiceImpl implements ElementService {
 
     private final ElementRepository repository;
     private final ElementMapper mapper;
-    private final DefectService defectService;
-    private final PlaceService placeService;
 
     @Override
     public List<ElementDto> save(List<NewElementDto> elementsDto) {
-        List<Element> elements = new ArrayList<>();
-        for (NewElementDto elementDto : elementsDto) {
-            Element element = mapper.mapToNewElement(elementDto);
-            if (elementDto.getDefects() != null) {
-                element.setDefects(defectService.save(elementDto.getDefects()));
-            }
-            if (elementDto.getPlaces() != null) {
-                element.setPlaces(placeService.save(elementDto.getPlaces()));
-            }
-            elements.add(element);
-        }
-        return mapper.mapToElementsDto(repository.saveAll(elements));
+        return mapper.mapToElementsDto(repository.saveAll(mapper.mapToNewElements(elementsDto)));
     }
 
     @Override
     public List<ElementDto> update(List<UpdateElementDto> elementsDto) {
         validateIds(elementsDto.stream().map(UpdateElementDto::getId).toList());
-        List<Element> elements = new ArrayList<>();
-        for (UpdateElementDto elementDto : elementsDto) {
-            Element element = mapper.mapToUpdateElement(elementDto);
-            if (element.getDefects() != null) {
-                element.setDefects(defectService.update(elementDto.getDefects()));
-            }
-            if (elementDto.getPlaces() != null) {
-                element.setPlaces(placeService.update(elementDto.getPlaces()));
-            }
-            elements.add(element);
-        }
-        return mapper.mapToElementsDto(repository.saveAll(elements));
+        return mapper.mapToElementsDto(repository.saveAll(mapper.mapToUpdateElements(elementsDto)));
     }
 
     private void validateIds(List<Long> ids) {
         Map<Long, Element> elements = repository.findAllById((ids))
-                .stream().collect(Collectors.toMap(Element::getId, d -> d));
+                .stream().collect(Collectors.toMap(Element::getId, n -> n));
         if (elements.size() != ids.size() || elements.isEmpty()) {
             List<Long> idsDb = new ArrayList<>(elements.keySet());
             ids = ids.stream().filter(e -> !idsDb.contains(e)).collect(Collectors.toList());
